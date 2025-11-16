af0c539ddaf63afba31eea6bca0c73ea57cf3fdf
AKW-MAX <agneswanini066l@gmail.com>
2025-11-16 07:16:22 -0800

    images moved to public

--- laset/src/components/Sections/Hero.jsx
+++ laset/src/components/Sections/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
 		 w-screen absolute h-150 saturate-70 sm:h-[32rem] md:h-[40rem] lg:h-[48rem] 
              grayscale-25
                     object-cover"
-		  src = "src/assets/images/HeroPic.png" alt = "Tomatoes being harvested" />
+		  src = "/images/HeroPic.png" alt = "Tomatoes being harvested" />
 		 <Navigation />
 		     <div className="text-center relative text-white p-2 mt-25 ">
 		<Header />
