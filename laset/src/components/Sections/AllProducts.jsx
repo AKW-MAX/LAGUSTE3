ff3c8b4baa1d6186ce409d1fb375ae674805b8db
AKW-MAX <agneswanini066l@gmail.com>
2025-11-16 09:50:37 -0800

    responsiveness on small screens

--- laset/src/components/Sections/AllProducts.jsx
+++ laset/src/components/Sections/AllProducts.jsx
@@ -18,77 +18,95 @@ export default function AllProducts() {
                     grid-cols-1 
                     sm:grid-cols-2 
                     md:grid-cols-3 
-                    lg:grid-cols-4 
+                    lg:grid-cols-4
+                    place-items-center
                     gap-6 
-                    px-4 sm:px-8 md:px-12 
+                    px-2 sm:px-6 md:px-10 
                     mb-10 mt-10
                 "
             >
-                <ProductsCard
-                    imgSrc="/images/Kelpreal.png"
-                    imgAlt="capsicum image"
-                    add="/images/add.png"
-                    name="Kelpreal 250ml"
-                    price="sh600"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Kelpreal.png"
+                        imgAlt="capsicum image"
+                        add="/images/add.png"
+                        name="Kelpreal 250ml"
+                        price="sh600"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Actellic.png"
-                    add="/images/add.png"
-                    imgAlt="Actellic image"
-                    name="Actellic 1L"
-                    price="sh6000"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Actellic.png"
+                        add="/images/add.png"
+                        imgAlt="Actellic image"
+                        name="Actellic 1L"
+                        price="sh6000"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Atom.png"
-                    add="/images/add.png"
-                    imgAlt="Atom image"
-                    name="Atom 1L"
-                    price="sh1700"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Atom.png"
+                        add="/images/add.png"
+                        imgAlt="Atom image"
+                        name="Atom 1L"
+                        price="sh1700"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Bedlam.png"
-                    add="/images/add.png"
-                    imgAlt="Bedlam image"
-                    name="Bedlam 50ml"
-                    price="sh400"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Bedlam.png"
+                        add="/images/add.png"
+                        imgAlt="Bedlam image"
+                        name="Bedlam 50ml"
+                        price="sh400"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Dynamo.png"
-                    add="/images/add.png"
-                    imgAlt="Dynamo image"
-                    name="Dynamo 100g"
-                    price="sh900"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Dynamo.png"
+                        add="/images/add.png"
+                        imgAlt="Dynamo image"
+                        name="Dynamo 100g"
+                        price="sh900"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Fendona.png"
-                    add="/images/add.png"
-                    imgAlt="Fendona image"
-                    name="Fendona 250ml"
-                    price="sh2200"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Fendona.png"
+                        add="/images/add.png"
+                        imgAlt="Fendona image"
+                        name="Fendona 250ml"
+                        price="sh2200"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/Lancer.png"
-                    add="/images/add.png"
-                    imgAlt="Lancer image"
-                    name="Lancer 1L"
-                    price="sh2800"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/Lancer.png"
+                        add="/images/add.png"
+                        imgAlt="Lancer image"
+                        name="Lancer 1L"
+                        price="sh2800"
+                    />
+                </div>
 
-                <ProductsCard
-                    imgSrc="/images/SpiderPlant.png"
-                    add="/images/add.png"
-                    imgAlt="SpiderPlant image"
-                    name="Spider Plant 500g"
-                    price="sh1900"
-                />
+                <div className="w-[90%] sm:w-full max-w-[260px]">
+                    <ProductsCard
+                        imgSrc="/images/SpiderPlant.png"
+                        add="/images/add.png"
+                        imgAlt="SpiderPlant image"
+                        name="Spider Plant 500g"
+                        price="sh1900"
+                    />
+                </div>
             </div>
         </>
     );
 }
+
 
--- laset/src/components/Sections/SignUp.jsx
+++ laset/src/components/Sections/SignUp.jsx
@@ -6,32 +6,37 @@ export default function SignUp() {
             <title>Sign up</title>
 
             <div className="relative">
+
                 {/* Image */}
                 <img
-                    className="w-80 h-64 absolute p-6 ml-11 sm:w-64 sm:h-52 md:w-72 md:h-60 lg:w-80 lg:h-64"
+                    className="w-64 h-52 sm:w-80 sm:h-64 absolute p-6 left-0 sm:left-auto"
                     src="/images/SignupPic.png"
                     alt="farmer images"
                 />
 
                 {/* Text Section */}
-                <div className="text-justify text-white bg-green-900 pr-4 pl-4 pb-12 pt-8 mt-4 sm:mt-6 md:mt-8 lg:mt-4">
-                    <h2 className="font-extrabold text-2xl mb-2 ml-150 sm:ml-4 md:ml-20 lg:ml-150 mt-3">
+                <div className="text-justify text-white bg-green-900 pr-4 pl-4 pb-12 pt-8 mt-4 sm:mt-4">
+                    <h2 className="font-extrabold text-2xl mb-2 ml-0 sm:ml-150 mt-3 text-center sm:text-left">
                         Sign Up for Exclusive Offers
                     </h2>
-                    <p className="font-semibold mb-1 ml-150 sm:ml-4 md:ml-20 lg:ml-150">
+                    <p className="font-semibold mb-1 ml-0 sm:ml-150 text-center sm:text-left">
                         Join our community and stay updated on the latest products <br />and promotions!
                     </p>
                     <input
                         type="email"
                         placeholder="Enter your email"
-                        className="text-black text-center rounded-lg pr-20 pb-2 ml-150 sm:ml-4 md:ml-20 lg:ml-150 bg-white mt-4 w-64 sm:w-48 md:w-60 lg:w-64"
+                        className="text-black text-center rounded-lg pr-20 pb-2 ml-0 sm:ml-150 bg-white mt-4 w-full sm:w-64"
                     />
-                    <Buttons />
+                    <div className="flex justify-center sm:justify-start mt-4">
+                        <Buttons />
+                    </div>
                 </div>
+
             </div>
         </>
     );
 }
+
 
 
 
