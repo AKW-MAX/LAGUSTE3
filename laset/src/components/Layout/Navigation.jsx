5172ef919fb265dbd69cd827b772ce3dfdf7243c
AKW-MAX <agneswanini066l@gmail.com>
2026-06-19 01:32:37 -0700

    made the navbar remain on top

--- laset/src/components/Layout/Navigation.jsx
+++ laset/src/components/Layout/Navigation.jsx
@@ -5,15 +5,21 @@ import { assets } from '../../assets/assets.js';
 function Navigation() {
   const cart = useSelector((state) => state.cart);
   return (
-    <div className="flex justify-between gap-2 text-center font-bold text-white relative p-4 mt-2 mb-2">
+    <div className="flex justify-between gap-2 text-center font-bold text-white p-4 w-full sticky top-0 z-50
+     shadow-md  bg-green-900
+
+    ">
       <div className="flex justify-between items-center gap-4 w-full ml-11 mr-11 
-                      flex-col sm:flex-row"> 
+                      flex-col "> 
         {/* STACK on small screens */}
 
-        <p className="ml-4">QualityFirst</p>
+        <p className="ml-4
+        sm:
+        
+        ">QualityFirst</p>
 
         <div className="flex justify-between items-center gap-4 ml-11 
-                        flex-col sm:flex-row"> 
+                        flex-col"> 
           {/* STACK on small screens */}
           <input type="text" placeholder="Search Items" className="bg-white text-green-700 border border-green-700 focus:outline-2 focus:outline-offset-1" />
 
