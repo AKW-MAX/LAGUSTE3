1a87b14baca20848260ce5ae2c306c4f109440a9
AKW-MAX <agneswanini066l@gmail.com>
2026-07-08 08:34:56 -0700

    fixed the shop button

--- laset/src/components/Common/Buttons.jsx
+++ laset/src/components/Common/Buttons.jsx
@@ -5,11 +5,8 @@ export default function Buttons() {
     <>
       <Link to="AllProducts">
         <button
-          className="
-          bg-green-950
-			px-3 py-1
-			sm:px-4 sm:py-2
-			md:px-5 md:py-2
+      className="
+     bg-green-950
 			rounded-lg
 			text-xs sm:text-sm md:text-base
 			font-semibold
